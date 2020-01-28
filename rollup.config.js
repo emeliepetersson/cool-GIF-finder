032913fc02882df453c651f0ebce731258bd76b3
emeliepetersson <emepet1204@skola.goteborg.se>
2020-01-28 11:28:47 +0100

    add babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
 import browsersync from "rollup-plugin-browsersync";
 import { terser } from "rollup-plugin-terser"; //med måsvingarna kan man hämta ut en specifik variabel från pluginet
 import postcss from "rollup-plugin-postcss";
+import babel from "rollup-plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import filesize from "rollup-plugin-filesize";
 
 const postcssNormalize = require("postcss-normalize");
 const autoprefixer = require("autoprefixer");
@@ -13,6 +17,8 @@ const isDevelopment = isProduction === false;
 console.log("is produktion: ", isProduction);
 console.log("is development: ", isDevelopment);
 
+//Blanda helst inte import och require.
+//Kör man import skriver man "export default {}" istället för "module.export{}"
 module.exports = {
   input: "src/scripts/index.js",
   output: {
@@ -21,11 +27,14 @@ module.exports = {
   },
   plugins: [
     isDevelopment && browsersync({ server: "public", watch: true }),
-    isProduction && terser(),
+    isProduction && terser() && filesize(),
     postcss({
       extract: true,
       sourceMap: isDevelopment,
       plugins: [postcssNormalize(), autoprefixer(), cssNano()]
-    })
+    }),
+    babel(),
+    resolve(),
+    commonjs()
   ]
 };
