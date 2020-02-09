d404dc2e0dc17497f6374c17b6010a2205bad7ea
emeliepetersson <emepet1204@skola.goteborg.se>
2020-02-09 11:23:31 +0100

    clean up code

--- src/scripts/giphy.js
+++ src/scripts/giphy.js
@@ -1,34 +1,16 @@
 "use strict";
 
+// Search for GIFs on Giphy.
 const search = input => {
-  fetch(
-    "https://api.giphy.com/v1/gifs/search?api_key=" +
-      process.env.GIPHY_API_KEY +
-      "&q=" +
-      input
-  )
+  return window
+    .fetch(
+      "https://api.giphy.com/v1/gifs/search?api_key=" +
+        process.env.GIPHY_API_KEY +
+        "&q=" +
+        input
+    )
     .then(response => response.json())
-    .then(gifs => {
-      console.log(gifs.data);
-
-      const gifsData = gifs.data;
-      if (gifsData.length === 0) {
-        const notFound = document.querySelector(".not-found");
-        notFound.classList.add("not-found--active");
-        notFound.textContent =
-          "We couldn't find any GIFs with the search query " + input + ".";
-      } else {
-        gifsData.forEach(gif => {
-          const gallery = document.querySelector(".gallery");
-          const a = document.createElement("a");
-          a.classList.add("gallery__item");
-          const img = new Image();
-          img.src = gif.images.downsized.url;
-          a.appendChild(img);
-          gallery.appendChild(a);
-        });
-      }
-    });
+    .catch(console.error);
 };
 
-export default search;
+export default { search };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,49 @@
 import "../styles/index.scss";
-import search from "./giphy.js";
+import giphy from "./giphy.js";
 
-const button = document.querySelector(".search__button");
-const input = document.querySelector(".search__field");
-
-button.addEventListener("click", event => {
+/**
+ * When the user submit and searches.
+ *
+ * @return {void}
+ */
+const onSubmit = event => {
   event.preventDefault();
-  const inputValue = input.value;
-  search(inputValue);
+
+  // Fetch the query from the input field.
+  const input = document.querySelector(".search__field");
+  const query = input.value;
+
+  giphy.search(query).then(gifs => {
+    const gifsData = gifs.data;
+    const notFound = document.querySelector(".not-found");
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+
+    notFound.classList.remove("not-found--active");
+
+    // If we can't find any items based on the search query we need to tell
+    // the user that.
+    if (gifsData.length === 0) {
+      notFound.classList.add("not-found--active");
+      notFound.textContent =
+        "We couldn't find any GIFs with the search query " + query + ".";
+    }
+
+    // Create new gallery items for all GIFs.
+    gifsData.forEach(gif => {
+      const a = document.createElement("a");
+      a.classList.add("gallery__item");
+      const img = new Image();
+      img.src = gif.images.downsized.url;
+
+      a.appendChild(img);
+      gallery.appendChild(a);
+    });
+  });
+};
+
+// Start the engine.
+window.addEventListener("load", () => {
+  const form = document.forms.search;
+  form.addEventListener("submit", onSubmit);
 });
